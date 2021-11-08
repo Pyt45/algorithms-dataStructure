822d3564a78eeb993c0e911ad70812b8af6d9885
pyt45 <hossamaqlzim107@gmail.com>
2021-11-09 00:17:10 +0100

    Add push front method to DoubleLinkedList

--- DataStructure/List/DoubleLinkedList.hpp
+++ DataStructure/List/DoubleLinkedList.hpp
@@ -38,6 +38,30 @@ namespace nds {
 					node->__prev = tmp;
 				}
 			}
+			void push_front(__pointer node) {
+				if (!this->__prev) {
+					this->__prev = node;
+					node->__next = this;
+				} else {
+					__pointer tmp = this;
+					while (tmp && tmp->__prev)
+						tmp = tmp->__prev;
+					tmp->__prev = node;
+					node->__next = tmp;
+				}
+			}
+			__pointer __get_head() {
+				__pointer tmp = this;
+				while (tmp && tmp->__prev)
+					tmp = tmp->__prev;
+				return tmp;
+			}
+			__pointer __get_tail() {
+				__pointer tmp = this;
+				while (tmp && tmp->__next)
+					tmp = tmp->__next;
+				return tmp;
+			}
 	};
 	template < class T, class Alloc = std::allocator<T> >
 	class DoubleLinkedList {
@@ -66,11 +90,26 @@ namespace nds {
 						__size++;
 					} else {
 						__head->push_back(node);
+						__tail = __head->__get_tail();
 						__size++;
 					}
 				}
 			}
-			void push_front(const T& val) {}
+			void push_front(const T& val) {
+				__pointer node = __alloc.allocate(1);
+				__alloc.construct(node, val);
+				if (node) {
+					if (__tail == nullptr) {
+						__head = __tail = node;
+						__size++;
+					}
+					else {
+						__tail->push_front(node);
+						__head = __tail->__get_head();
+						__size++;
+					}
+				}
+			}
 			void traverse_forward() {
 				__pointer tmp = __head;
 				while (tmp) {
