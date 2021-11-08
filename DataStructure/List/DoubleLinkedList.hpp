66640d0c4f245e9f383cfa2e1642a9b1606b26f1
pyt45 <hossamaqlzim107@gmail.com>
2021-11-08 23:19:32 +0100

    Add push back to Double linked List

--- DataStructure/List/DoubleLinkedList.hpp
+++ DataStructure/List/DoubleLinkedList.hpp
@@ -28,6 +28,51 @@ namespace nds {
 	};
 	template < class T, class Alloc = std::allocator<T> >
 	class DoubleLinkedList {
-		
+		public:
+			typedef T value_type;
+			typedef size_t size_type;
+			typedef Node<T>* __pointer;
+			typedef typename Alloc::template rebind< Node<T> >::other __node_allocator;
+		private:
+			__node_allocator __alloc;
+			size_type __size;
+			__pointer __head;
+			__pointer __tail;
+		public:
+			DoubleLinkedList() {
+				__head = __tail = nullptr;
+				__size = 0;
+			}
+			~DoubleLinkedList() {}
+			void push_back(const T& val) {
+				__pointer node = __alloc.allocate(1);
+				__alloc.construct(node, val);
+				if (node) {
+					if (__head == nullptr) {
+						__head = __tail = node;
+						__size++;
+					}
+				}
+			}
+			void push_front(const T& val) {}
+			void traverse_forward() {
+				__pointer tmp = __head;
+				while (tmp) {
+					std::cout << tmp->__data << std::endl;
+					tmp = tmp->__next;
+				}
+			}
+			void traverse_backward() {
+				__pointer tmp = __tail;
+				while (tmp) {
+					std::cout << tmp->__data << std::endl;
+					tmp = tmp->__prev;
+				}
+			}
+			__pointer find(const T& val);
+			void destory_node(const T& val);
+			size_type size() const {
+				return __size;
+			}
 	};
 }
