be21819a4761bb8bad8e448bd31a013023f7d1e5
pyt45 <hossamaqlzim107@gmail.com>
2021-11-09 00:19:10 +0100

    Add push front Test

--- DataStructure/Tests/DoubleLinkedListTest.cpp
+++ DataStructure/Tests/DoubleLinkedListTest.cpp
@@ -3,10 +3,20 @@
 
 int main() {
     nds::DoubleLinkedList<int> dl;
+    nds::DoubleLinkedList<int> dl1;
+
+    std::cout << "==== push back test ====\n";
 
     dl.push_back(1);
     dl.push_back(2);
     dl.push_back(3);
     dl.push_back(4);
     dl.traverse_forward();
+    std::cout << "==== push back test ====\n";
+    dl1.push_front(1);
+    dl1.push_front(2);
+    dl1.push_front(3);
+    dl1.push_front(4);
+
+    dl1.traverse_forward();
 }
